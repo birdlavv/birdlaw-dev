dec926898361c179b2e447bdf3bd88cae7f34ef3
Alex Fong <alexfong22@gmail.com>
2025-10-08 12:55:51 -0500

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,27 @@
 {
-  description = "Dev env with node 24, pnpm, and biome";
+  description = "Dev env with node 24, pnpm, & biome";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      system = "x86_64-linux"; # or "aarch64-darwin" for macOS arm64
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs_24
-          pkgs.biome
-          pkgs.nodePackages.pnpm
-        ];
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_24
+            pkgs.biome
+            pkgs.git
+            pkgs.nodePackages.pnpm
+          ];
 
-        shellHook = ''
-          echo "Dev shell with Node.js $(node -v), pnpm $(pnpm -v), biome $(biome version)"
-        '';
-      };
-    };
+          shellHook = ''
+            echo "Dev shell with node $(node -v), pnpm $(pnpm -v), biome"
+          '';
+        };
+      });
 }
